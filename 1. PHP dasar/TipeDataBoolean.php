dea4cc0fd5a45192d90b916176fbdccc6a8d6999
abdul <malik301002@gmail.com>
2021-10-31 09:44:21 +0700

    menambahkan TipeDataBoolean - PHP dasar

---
+++ 1. PHP dasar/TipeDataBoolean.php
@@ -0,0 +1,17 @@
+<?php
+
+//* 1. PHP Dasar
+//* 1.3. Tipe data boolean pada PHP
+
+//? Tipe data boolean adalah tipe data dengan nilai kebenaran (benar atau salah)
+//? Nilai benar direpresentasikan dengan "true" (case insensitive)
+//? Nilai salah direpresentasikan dengan "false" (case insensitive)
+
+// true
+echo "Benar : ";
+var_dump(true);
+
+// false
+// contoh dengan case insensitive
+echo "Salah : ";
+var_dump(faLSE);
